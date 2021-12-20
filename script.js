bcc3abc84dd1230d7466db7fd9d8fb7d5ec434cf
danillo <danillogonvalves001@gmail.com>
2021-12-20 16:23:53 -0300

    Quinto requisito feito.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let colors = []
 const textColorGuess = document.querySelector('#rgb-color');
 const colorsForChoices = document.querySelector('#colors-for-choices');
+const answer = document.querySelector('#answer');
 
 function colorRandom() {
     let color = `(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`;
@@ -32,10 +33,21 @@ function createRiddle() {
     for (let i = functionColors.length - 1; i >= 0; i -= 1) {
         const colorToChoose = document.createElement('div');
         colorToChoose.classList.add('ball');
+        colorToChoose.addEventListener('click', checkTheAnswer);
         colorToChoose.style.backgroundColor = `rgb${functionColors.splice((Math.random()*functionColors.length), 1)}`;
         colorsForChoices.appendChild(colorToChoose);
     }
+    answer.innerHTML = "Escolha uma cor";
 }
-
+function checkTheAnswer(e) {
+    const question = e.target.style.backgroundColor;
+    console.log(question)
+    const correct = `rgb${textColorGuess.innerHTML}`;
+    if (question === correct) {
+        answer.innerHTML = "Acertou!";
+    } else {
+        answer.innerHTML = "Errou! Tente novamente!";
+    }
+}
 mysteriousColor()
 createRiddle()
