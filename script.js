d01dee5016a35d71ec81f060dc857d886d9a7f7a
danillo <danillogonvalves001@gmail.com>
2021-12-28 19:06:13 -0300

    Arrumando o ESLint.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let colors = []
+let colors = [];
 const textColorGuess = document.querySelector('#rgb-color');
 const colorsForChoices = document.querySelector('#colors-for-choices');
 const answer = document.querySelector('#answer');
@@ -7,58 +7,62 @@ const score = document.querySelector('#score');
 let numberScore = 0;
 score.innerHTML = `Placar: ${numberScore}`;
 
+function generatesRGB() {
+  return Math.floor(Math.random() * 256);
+}
+
 function colorRandom() {
-    let color = `(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`;
-    return color;
+  const color = `(${generatesRGB()}, ${generatesRGB()}, ${generatesRGB()})`;
+  return color;
 }
 function mysteriousColor() {
-    colors = [];
-    const colorGuess = colorRandom();
-    const color1 = colorRandom();
-    const color2 = colorRandom();
-    const color3 = colorRandom();
-    const color4 = colorRandom();
-    const color5 = colorRandom();
-    colors.push(colorGuess, color1, color2, color3, color4, color5);
+  colors = [];
+  const colorGuess = colorRandom();
+  const color1 = colorRandom();
+  const color2 = colorRandom();
+  const color3 = colorRandom();
+  const color4 = colorRandom();
+  const color5 = colorRandom();
+  colors.push(colorGuess, color1, color2, color3, color4, color5);
 }
 
+function checkTheAnswer(e) {
+  const question = e.target.style.backgroundColor;
+  const correct = `rgb${textColorGuess.innerHTML}`;
+  if (question === correct) {
+    answer.innerHTML = 'Acertou!';
+    numberScore += 3;
+    score.innerHTML = `Placar: ${numberScore}`;
+  } else {
+    answer.innerHTML = 'Errou! Tente novamente!';
+    numberScore -= 1;
+    score.innerHTML = `Placar: ${numberScore}`;
+  }
+}
 
 function createRiddle() {
-    let functionColors = colors;
-    textColorGuess.innerHTML = functionColors[0];
-    for (let i = functionColors.length - 1; i >= 0; i -= 1) {
-        const colorToChoose = document.createElement('div');
-        colorToChoose.classList.add('ball');
-        colorToChoose.addEventListener('click', checkTheAnswer);
-        colorToChoose.style.backgroundColor = `rgb${functionColors.splice((Math.random()*functionColors.length), 1)}`;
-        colorsForChoices.appendChild(colorToChoose);
-    }
-    answer.innerHTML = "Escolha uma cor";
-}
-function checkTheAnswer(e) {
-    const question = e.target.style.backgroundColor;
-    const correct = `rgb${textColorGuess.innerHTML}`;
-    if (question === correct) {
-        answer.innerHTML = "Acertou!";
-        numberScore += 3;
-        score.innerHTML = `Placar: ${numberScore}`;
-    } else {
-        answer.innerHTML = "Errou! Tente novamente!";
-        numberScore -= 1;
-        score.innerHTML = `Placar: ${numberScore}`;
-    }
+  textColorGuess.innerHTML = [colors[0]];
+  for (let i = colors.length - 1; i >= 0; i -= 1) {
+    const colorToChoose = document.createElement('div');
+    colorToChoose.classList.add('ball');
+    colorToChoose.addEventListener('click', checkTheAnswer);
+    const randomArrayPosition = Math.random() * colors.length;
+    colorToChoose.style.backgroundColor = `rgb${colors.splice(randomArrayPosition, 1)}`;
+    colorsForChoices.appendChild(colorToChoose);
+  }
+  answer.innerHTML = 'Escolha uma cor';
 }
 
 function resetGame() {
-    for (let  i = colorsForChoices.children.length - 1; i >= 0; i -= 1) {
-        const ball = colorsForChoices.children[i];
-        colorsForChoices.removeChild(ball);
-    }
-    mysteriousColor()
-    createRiddle()
+  for (let i = colorsForChoices.children.length - 1; i >= 0; i -= 1) {
+    const ball = colorsForChoices.children[i];
+    colorsForChoices.removeChild(ball);
+  }
+  mysteriousColor();
+  createRiddle();
 }
 
-mysteriousColor()
-createRiddle()
+mysteriousColor();
+createRiddle();
 
-buttonResetGame.addEventListener('click', resetGame)
+buttonResetGame.addEventListener('click', resetGame);
